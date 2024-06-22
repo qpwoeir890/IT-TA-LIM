9a3ade53046335956f5cae6582a9498bc5091b3e
qpwoeir890 <x2702813@gmail.comgit config --global user.email x2702813@gmail.com>
2024-06-22 23:27:26 +0500

    first commit

---
+++ js/add.js
@@ -0,0 +1,8 @@
+document.getElementById("languageSelect").addEventListener("change", function() {
+    var selectedLanguage = this.value;
+    
+    // Здесь можно добавить логику для изменения языка на странице
+  });
+
+ 
+
