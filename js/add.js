b37ac8b51e73baacee1a302397adf3814735a44c
qpwoeir890 <x2702813@gmail.comgit config --global user.email x2702813@gmail.com>
2024-06-24 13:20:24 +0500

    gute finish

--- js/add.js
+++ js/add.js
@@ -4,5 +4,22 @@ document.getElementById("languageSelect").addEventListener("change", function() 
     // Здесь можно добавить логику для изменения языка на странице
   });
 
- 
+ document.addEventListener('DOMContentLoaded',  ()  =>  {
+
+  const mediaFiles = document.querySelectorAll('img, video');
+  let i = 0
+
+  Array.from(mediaFiles).forEach((file, index) => {
+    file.onload = () => {
+      i++
+
+      percents.innerHTML = ((i * 100) / mediaFiles.length).toFixed(1)
+
+      if(i === mediaFiles.length) {
+        preloader.classList.add('preloader--hide')
+        percents.innerHTML = 100
+      }
+    }
+  })
+ })
 
